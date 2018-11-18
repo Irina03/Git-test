ff21fbacd9933b1f74e16a4e0fb339d8ee8fe615
IRISKA <ymalos@protonmail.com>
2018-11-18 20:24:37 +0200

    java

---
+++ Cont.java
@@ -0,0 +1,22 @@
+﻿import java.util.Scanner*;
+
+public class Cont{
+public static void main(String args[]) {
+int i;
+int k;
+
+public void Scan(){
+System.out.println("Введите первое число");
+Scanner scn = new Scanner(System.in);
+i = scn.nextInt();
+System.out.println("Введите второе число");
+k = scn.nextInt();
+}
+
+private static int count(int x){
+x= x+1;
+return x;
+}
+}
+
+}
